1e17b3bdbfc07bb9f5d87e4e779e19ff26c36f34
egorrozinskiy <egor.rozinskiy@mail.ru>
2016-09-22 17:18:28 +0300

    done

--- lab04/task_13.py
+++ lab04/task_13.py
@@ -5,7 +5,19 @@ from pyrob.api import *
 
 @task
 def task_8_10():
-    pass
+    while True:
+        if  not wall_is_above() :
+            move_up()
+            fill_cell()
+            move_down()
+        if  not wall_is_beneath() :
+            move_down()
+            fill_cell()
+            move_up()
+        if not wall_is_on_the_right():
+            move_right()
+        else:
+            break
 
 
 if __name__ == '__main__':
--- lab04/task_15.py
+++ lab04/task_15.py
@@ -5,8 +5,21 @@ from pyrob.api import *
 
 @task
 def task_8_21():
-    pass
-
-
+    if wall_is_on_the_left() and wall_is_above():
+        while not wall_is_on_the_right() and not wall_is_beneath():
+           move_right()
+           move_down()
+    elif wall_is_beneath() and wall_is_on_the_left():
+        while not wall_is_on_the_right() and not wall_is_above():
+           move_right()
+           move_up()
+    elif wall_is_above() and wall_is_on_the_right():
+        while not wall_is_on_the_left() and not wall_is_beneath():
+           move_left()
+           move_down()
+    elif wall_is_beneath() and wall_is_on_the_right():
+        while not wall_is_on_the_left() and not wall_is_above():
+           move_left()
+           move_up()
 if __name__ == '__main__':
     run_tasks()
