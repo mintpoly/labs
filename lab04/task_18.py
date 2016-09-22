cbea1a74f1de50921b569d4bacef10b849cd575b
egorrozinskiy <egor.rozinskiy@mail.ru>
2016-09-22 17:57:42 +0300

    done

--- lab04/task_18.py
+++ lab04/task_18.py
@@ -5,8 +5,20 @@ from pyrob.api import *
 
 @task
 def task_8_28():
-    pass
-
-
+    while not wall_is_on_the_left() and wall_is_above() and wall_is_beneath():
+        move_left()
+    if not wall_is_above():
+        while not wall_is_above():
+            move_up()
+        while not wall_is_on_the_left():
+            move_left()
+    else:
+        while not wall_is_on_the_right() and wall_is_above() and wall_is_beneath():
+            move_right()
+    if not wall_is_above():
+        while not wall_is_above():
+            move_up()
+        while not wall_is_on_the_left():
+            move_left()
 if __name__ == '__main__':
     run_tasks()
