1568a0e5a49b00cd94bf3577ad000f58e0b56995
egorrozinskiy <egor.rozinskiy@mail.ru>
2016-09-22 18:13:25 +0300

    done

--- lab04/task_19.py
+++ lab04/task_19.py
@@ -5,7 +5,21 @@ from pyrob.api import *
 
 @task
 def task_8_29():
-    pass
+    while not wall_is_on_the_left() and wall_is_above() and wall_is_beneath():
+        move_left()
+    if wall_is_on_the_left() and not wall_is_above():
+        while not wall_is_above():
+            move_up()
+        while not wall_is_on_the_left():
+            move_left()
+    else:
+        while not wall_is_on_the_right() and wall_is_above() and wall_is_beneath():
+            move_right()
+    if wall_is_on_the_right() and not wall_is_above():
+        while not wall_is_above():
+            move_up()
+        while not wall_is_on_the_left():
+            move_left()
 
 
 if __name__ == '__main__':
--- lab04/task_20.py
+++ lab04/task_20.py
@@ -5,7 +5,19 @@ from pyrob.api import *
 
 @task(delay=0.05)
 def task_4_3():
-    pass
+    move_right()
+    for i in range (6):
+        for i in range (27):
+            fill_cell()
+            move_right()
+        move_down()
+        move_left()
+        for i in range (27):
+            fill_cell()
+            move_left()
+        move_down()
+        move_right()
+
 
 
 if __name__ == '__main__':
